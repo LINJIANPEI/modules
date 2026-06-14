7d444ba273a59a9f8aa3d902ed08abeafeaf6d71
Linlin <1732509260@qq.com>
2026-06-15 01:37:18 +0800

    更新

--- customize.sh
+++ customize.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
-
+ui_print "********************************************"
 ui_print "- 正在检测Hosts模块"
+ui_print "********************************************"
+
 found_hosts=false
 for module in /data/adb/modules/*; do
     [ -f "${module}/system/etc/hosts" ] && [ -f "${module}/module.prop" ] && {
@@ -10,6 +12,40 @@ for module in /data/adb/modules/*; do
     }
 done
 [ "$found_hosts" = true ] && ui_print "- 冲突模块已标记移除，安装完成后请重启设备。"
+
+
+
+
+preserve_existing_config() {
+    local module_name=$(basename "$MODPATH")
+    local old_file="$1"
+    local new_file="${2:-$1}"  # 第二个参数默认等于第一个
+    
+    local old_config_path="/data/adb/modules/${module_name}/${old_file}"
+    local new_config_path="$MODPATH/${new_file}"
+    
+    if [ -f "$old_config_path" ]; then
+        # 如果新配置文件存在，则备份
+        if [ -f "$new_config_path" ]; then
+            mv "$new_config_path" "${new_config_path}.bak"
+        fi
+        cp "$old_config_path" "$new_config_path"
+        ui_print "✓ 已保留现有配置文件: ${old_file}"
+        return 0
+    else
+        ui_print "! 未找到现有配置: ${old_file}，使用默认配置"
+        return 1
+    fi
+}
+
+preserve_existing_config "config.conf"
+preserve_existing_config "conf/AdGuardHome.yaml"
+preserve_existing_config "conf/applist.prop"
+preserve_existing_config "conf/mihomo.yaml"
+preserve_existing_config "conf/oxidns.yaml"
+preserve_existing_config "conf/smartdns.conf"
+
+
 
 set_perm $MODPATH/service.sh 0 0 0755
 set_perm $MODPATH/action.sh 0 0 0755
@@ -30,3 +66,8 @@ set_perm $MODPATH/scripts/clearLog.sh 0 0 0755
 set_perm $MODPATH/scripts/clash.sh 0 0 0755
 set_perm $MODPATH/scripts/action.sh 0 0 0755
 set_perm $MODPATH/scripts/oiface.sh 0 0 0755
+
+ui_print "********************************************"
+ui_print "安装完成！"
+ui_print "********************************************"
+
